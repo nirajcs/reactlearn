f76ff96281292dd2dad984707b186710c57bd3b6
Niraj <ncs7498@gmail.com>
2023-07-15 16:18:08 +0530

    My todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,112 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let weeks = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  let dayOfWeek = new Date().getDay();
+  let today = weeks[dayOfWeek]
+  const [todo,setTodo] = useState('')
+  const [todos,setTodos] = useState([])
+
+  let updateTodo = (e)=>{
+    setTodo(e.target.value)
+  }
+
+  let pushTodo = (e)=>{
+    e.preventDefault()
+    if(todo===''){
+      return
+    }
+    setTodos([...todos,{id:Date.now(),text:todo,status:false}]);
+    setTodo('');
+  }
+
+  let statusChange = (item, e) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((obj) => {
+        if (obj.id === item.id) {
+          return { ...obj, status: e.target.checked };
+        }
+        return obj;
+      });
+    });
+  };
+  
+  let deleteTask = (taskId) =>{
+    setTodos(
+      todos.filter(item => item.id !== taskId)
+    )
+    return null
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App container-fluid">
+      <div className="main">
+        <div className="title">
+        <div className="card">
+          <div className="card-body">
+            <h2 style={{color: '#331d2c'}}><b>TODO APP</b></h2>
+            <h5>Hey it's {today} 🌝 ☕</h5>
+          </div>
+        </div>
+        </div>
+        <div className="content row mt-4">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-body">
+                <form onSubmit={pushTodo}>              
+                  <div className="input-group mb-3">
+                    <input type="text" className="form-control form-control-lg" value={todo} onChange={updateTodo} placeholder="Enter your task..."/>
+                    <div className="input-group-append">
+                      <button className="btn btn-lg btn-dark" type="submit">Add</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+                {
+                  todos.map((item)=>{
+                    return(
+                      <div className="card todo-list m-2 p-2" key={item.id}>
+                        <div className="todos d-flex justify-content-between">
+                          <div className="input-group">
+                            <div className="input-group-prepend">
+                              <div className="input-group-text">
+                                <input className='m-0' type="checkbox" value={item.status} onChange={(e)=>{statusChange(item,e)}}/>
+                              </div>
+                            </div>
+                            <p className='m-0'>{item.text}</p>
+                          </div>
+                          <i onClick={()=>deleteTask(item.id)} className="fa-sharp fa-solid fa-trash" style={{ color: '#331d2c' }}></i>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+            </div>
+          </div>
+          <div className="col-md-6"> 
+            <div className="card">
+              <div className="card-body">
+                <h4 className='mb-4'>Tasks Completed</h4>
+                {
+                  todos.map((item) => {
+                    if (item.status) {
+                      return (
+                        <div className="card todo-list m-2" key={item.id}>
+                          <p className='my-2'>{item.text}</p>
+                        </div>
+                      );
+                    }
+                    return null;
+                  })
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
   );
 }
 
